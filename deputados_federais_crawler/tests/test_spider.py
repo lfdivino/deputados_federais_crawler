8c7516d29308d7e99e6834d40da04d49c1b46fe9
Luiz Felipe <lf.divino@gmail.com>
2018-02-08 15:32:40 -0200

    [REF] Spider test case

--- deputados_federais_crawler/tests/test_spider.py
+++ deputados_federais_crawler/tests/test_spider.py
@@ -14,17 +14,6 @@ from deputados_federais_crawler.deputados_federais_crawler.spiders.\
 class TestSpiders(unittest.TestCase):
     def setUp(self):
         self.execute_crawler()
-        self.congressmen_json = self.congressmen_data_json()
-
-    def congressmen_data_json(self):
-        congressmen_list = []
-        with open('../congressmen_data.json', 'r') as file:
-            dados_file = file.read().split("\n")
-            for congressman in dados_file:
-                if congressman:
-                    congressmen_list.append(json.loads(congressman))
-
-        return congressmen_list
 
     def execute_crawler(self):
         crawler_settings = Settings()
@@ -33,30 +22,44 @@ class TestSpiders(unittest.TestCase):
         crawler_process.crawl(FederalCongressmenCrawler)
         crawler_process.start(stop_after_crawl=True)
 
-    def connect_to_collection(self):
+    def connect_to_mongodb_cluster(self):
         client = MongoClient(my_settings.MONGO_URI)
-        congressmen_db = client.congressmen
-        congressmen_collection = congressmen_db.congressmen
+
+        return client
+
+    def connect_db(self, client_cluster):
+        congressmen_db = client_cluster.congressmen
+
+        return congressmen_db
+
+    def connect_collection(self, db, collection_name):
+        congressmen_collection = db[collection_name]
 
         return congressmen_collection
 
-    def test_congressmen_not_null(self):
+    def test_congressmen_data(self):
+        client_cluster = self.connect_to_mongodb_cluster()
         self.assertIsNotNone(
-            self.congressmen_json,
-            "There aren't any congressmen data in the json file!"
-        )
-        self.assertEqual(
-            self.congressmen_json[0]['nome'],
-            "ABEL MESQUITA JR.",
-            "The nome of the first congressman isn't 'ABEL MESQUITA JR.'"
-        )
-        self.assertEqual(
-            self.congressmen_json[0]['gabinete'],
-            "248",
-            "The gabinete of the first congressman isn't '248"
+            client_cluster.nodes,
+            "There aren't any valid database!"
         )
 
-        congressmen_collection = self.connect_to_collection()
+        congressmen_db = self.connect_db(client_cluster)
+        self.assertEqual(
+            congressmen_db.collection_names()[0],
+            'congressmen',
+            "There isn't the correct collection!"
+        )
+        congressmen_collection = self.connect_collection(
+            congressmen_db, 'congressmen'
+        )
+
+        self.assertEqual(
+            congressmen_collection.find().count(),
+            513,
+            "The number of congresmen isn't correct!"
+        )
+
         self.assertIsNotNone(
             congressmen_collection.find_one({'nome': 'ABEL MESQUITA JR.'}),
             "There aren't any congressmen data in the database collection!"
