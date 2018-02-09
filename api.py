1cb26969d2989389939975029788edab8cb27fb3
Luiz Felipe <lf.divino@gmail.com>
2018-02-09 20:34:58 -0200

    [ADD] Dockerfile

--- api.py
+++ api.py
@@ -16,11 +16,6 @@ api = Api(app)
 data_base = DeputadosDb(
     os.getenv('MONGO_URI'), os.getenv('MONGO_DATABASE'), 'congressmen'
 )
-# data_base = DeputadosDb(
-#     'mongodb://api_user:apiuser2018#@cluster0-shard-00-00-xmzvt.mongodb.net:27017,cluster0-shard-00-01-xmzvt.mongodb.net:27017,cluster0-shard-00-02-xmzvt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
-#     'congressmen',
-#     'congressmen'
-# )
 
 
 class ScrapyDeputados(Resource):
@@ -76,4 +71,3 @@ api.add_resource(BuscarDeputadosEstado, '/api/v1/estado/<estado>')
 
 if __name__ == '__main__':
     run_simple('0.0.0.0', 5000, app)
-    #app.run(debug=True)
