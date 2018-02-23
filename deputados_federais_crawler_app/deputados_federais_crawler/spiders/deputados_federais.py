6eda8bc732fc47e255a525276f950c3858f1f621
Luiz Felipe <lf.divino@gmail.com>
2018-02-23 20:45:05 -0300

    [REF] Alterada a pagina para a busca dos deputados
    - Alterado o parse das informações dos deputados

--- deputados_federais_crawler_app/deputados_federais_crawler/spiders/deputados_federais.py
+++ deputados_federais_crawler_app/deputados_federais_crawler/spiders/deputados_federais.py
@@ -3,39 +3,38 @@ import scrapy
 
 class DeputadosFederaisCrawler(scrapy.Spider):
     name = "congressmen"
-    pagina_deputados = 'http://www.camara.leg.br/internet/deputado/Dep_Lista.asp?Legislatura=55&Partido=QQ&SX=QQ&Todos=None&UF=QQ&condic=QQ&forma=lista&nome=&ordem=nome&origem=None'
+    pagina_deputados = 'http://www.camara.leg.br/internet/deputado/Dep_Lista_foto.asp?Legislatura=55&Partido=QQ&SX=QQ&Todos=None&UF=QQ&condic=QQ&forma=lista&nome=&ordem=nome&origem=None'
     start_urls = [
         pagina_deputados,
     ]
 
     def parse_dados_deputado(self, deputado):
-        dados_deputados = deputado.css(
-            'li::text').extract()[2].strip('\r\n\t').split(' - ')
-
         nome = deputado.css('a > b::text').extract()[0]
-        partido = dados_deputados[0].split(':')[1].split('/')[0].strip()
-        uf = dados_deputados[0].split(':')[1].split('/')[1].strip()
-        gabinete = dados_deputados[1].split(':')[1].strip()
-        anexo = dados_deputados[2].split(':')[1].strip()
-        fone = dados_deputados[3].split(':')[1].strip()
-        fax = dados_deputados[4].split(':')[1].strip()
-        email = deputado.css('a::text').extract()[0]
+        foto = deputado.css('img::attr(src)').extract()[0]
+        partido = deputado.css('font::text')[0].extract().split('-')[0].strip()
+        uf = deputado.css('font::text')[0].extract().split('-')[1].strip()
+        gabinete = deputado.css('font::text')[1].extract().split(' ')[1]
+        anexo = deputado.css('font::text')[1].extract().split(' ')[3]
+        fone = deputado.css('font::text')[2].extract().split(' ')[1]
+        fax = deputado.css('font::text')[3].extract().split(' ')[1]
+        email = deputado.css('font::text')[4].extract()
 
-        return nome, partido, uf, gabinete, anexo, fone, fax, email
+        return nome, foto, partido, uf, gabinete, anexo, fone, fax, email
 
     def parse(self, response):
-        for deputado in response.css('#demaisInformacoes'):
+        for deputado in response.css("#depFoto > td"):
+            if not deputado.css('font::attr(color)'):
+                nome, foto, partido, uf, gabinete, anexo, fone, fax, email = \
+                    self.parse_dados_deputado(deputado)
 
-            nome, partido, uf, gabinete, anexo, fone, fax, email = \
-                self.parse_dados_deputado(deputado)
-
-            yield {
-                'nome': nome,
-                'partido': partido,
-                'uf': uf,
-                'gabinete': gabinete,
-                'anexo': anexo,
-                'fone': fone,
-                'fax': fax,
-                'email': email
-            }
+                yield {
+                    'nome': nome,
+                    'foto': foto,
+                    'partido': partido,
+                    'uf': uf,
+                    'gabinete': gabinete,
+                    'anexo': anexo,
+                    'fone': fone,
+                    'fax': fax,
+                    'email': email
+                }
