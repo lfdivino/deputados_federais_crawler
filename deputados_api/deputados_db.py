00956e14d8859b6c8027f840ca2683a74ab11a24
Luiz Felipe <lf.divino@gmail.com>
2018-02-23 00:50:25 -0300

    [FIX] Nomenclatura agora em português

--- deputados_api/deputados_db.py
+++ deputados_api/deputados_db.py
@@ -2,41 +2,41 @@ from pymongo import MongoClient
 
 
 class DeputadosDb(object):
-    def __init__(self, mongo_uri, mongo_database, collection_name):
+    def __init__(self, mongo_uri, mongo_database, nome_collection):
         self.mongo_uri = mongo_uri
         self.mongo_database = mongo_database
-        self.mongo_collection = collection_name
+        self.mongo_collection = nome_collection
         self.db_collection = self.conexao_banco_dados()
 
-    def connect_to_mongodb_cluster(self):
+    def conectar_mongodb_cluster(self):
         client = MongoClient(self.mongo_uri)
 
         return client
 
-    def connect_db(self, client_cluster):
-        congressmen_db = client_cluster.congressmen
+    def conectar_db(self, client_cluster):
+        deputados_db = client_cluster.congressmen
 
-        return congressmen_db
+        return deputados_db
 
-    def connect_collection(self, db, collection_name):
-        congressmen_collection = db[collection_name]
+    def conectar_collection(self, db, nome_collection):
+        deputados_collection = db[nome_collection]
 
-        return congressmen_collection
+        return deputados_collection
 
     def conexao_banco_dados(self):
-        client = self.connect_to_mongodb_cluster()
-        client_db = self.connect_db(client)
-        client_collection = self.connect_collection(
+        client = self.conectar_mongodb_cluster()
+        client_db = self.conectar_db(client)
+        client_collection = self.conectar_collection(
             client_db, self.mongo_collection
         )
 
         return client_collection
 
-    def processar_deputados(self, deputados_from_db):
+    def processar_deputados(self, deputados_db):
         deputados = {
             'Deputados': []
         }
-        for deputado in deputados_from_db:
+        for deputado in deputados_db:
             del deputado['_id']
             deputados['Deputados'].append(deputado)
 
@@ -46,13 +46,16 @@ class DeputadosDb(object):
         deputado_domain = {}
         if nome_deputado:
             deputado_domain['nome'] = nome_deputado
+            deputado_db = self.db_collection.find_one(deputado_domain)
+            if not deputado_db:
+                return "Deputado com este nome não foi encontrado!"
             deputados = {
-                'Deputados': self.db_collection.find_one(deputado_domain)
+                'Deputados': deputado_db
             }
             del deputados['Deputados']['_id']
         else:
-            deputados_from_db = self.db_collection.find(deputado_domain)
-            deputados = self.processar_deputados(deputados_from_db)
+            deputados_db = self.db_collection.find(deputado_domain)
+            deputados = self.processar_deputados(deputados_db)
 
         return deputados
 
@@ -63,13 +66,13 @@ class DeputadosDb(object):
         return deputados
 
     def buscar_partido(self, partido):
-        deputados_from_db = self.db_collection.find({'partido': partido})
-        deputados = self.processar_deputados(deputados_from_db)
+        deputados_db = self.db_collection.find({'partido': partido})
+        deputados = self.processar_deputados(deputados_db)
 
         return deputados
 
     def buscar_estado(self, estado):
-        deputados_from_db = self.db_collection.find({'uf': estado})
-        deputados = self.processar_deputados(deputados_from_db)
+        deputados_db = self.db_collection.find({'uf': estado})
+        deputados = self.processar_deputados(deputados_db)
 
         return deputados
